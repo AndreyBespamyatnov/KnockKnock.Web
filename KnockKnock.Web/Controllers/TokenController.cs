514e770c7681b26c9a10abead7cefc7310bc83dc
Andrei Bespamiatnov <adminsgz@gmail.com>
2016-11-24 23:49:16 +0300

    Add TokenController

---
+++ KnockKnock.Web/Controllers/TokenController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Web.Http;
+
+namespace KnockKnock.Web.Controllers
+{
+    /// <summary>
+    /// User token controller
+    /// </summary>
+    public class TokenController : ApiController
+    {
+        /// <summary>
+        /// Get your token.
+        /// </summary>
+        /// <returns>Your token</returns>
+        public IHttpActionResult Get()
+        {
+            try
+            {
+                var result = Guid.Empty;
+                return Ok(result);
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+    }
+}
